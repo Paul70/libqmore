9203defd32f24eb1c3c11a47ed07747437b85b71
Paul <paul-heidenreich@web.de>
2020-11-29 15:35:47 +0100

    started with new global project feature:  - cmake function "headerTest"

---
+++ CMakeModules/QMoreHeaderTesting.cmake
@@ -0,0 +1,102 @@
+# *******************************************************************************************************************************
+# QMoreHeaderTesting
+#
+# CMake function defintion "headerTest"
+# ARGV : a list of all subdirectories of project libqmore (added with add_subdirectory)
+#
+# 'headerTest' verifies that all project's header files are self-contained, i.e each header file compiles itself or, in other
+# words, a cpp file only including a single hader file compiles.
+#
+# This follows directly the device given on
+# https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rs-contained
+#
+# *******************************************************************************************************************************
+
+function(headerTest)
+    message(STATUS "Examining subdirectories: ${ARGV}")
+
+    # loop through all (master)project subdirectories
+    foreach(subdir ${ARGV})
+
+        # the subdirectory "headertest" only calls this cmake function
+        if(${subdir} STREQUAL ${CMAKE_CURRENT_SOURCE_DIR})
+            continue()
+        endif()
+
+        message(STATUS "Examining subdirectory: ${subdir}")
+
+        # get list of all subdirectory targets
+        get_property(target_list
+            DIRECTORY "${subdir}"
+            PROPERTY BUILDSYSTEM_TARGETS
+        )
+        message(STATUS "examining built targets: ${targets_list} in subdir ${subdir}")
+
+        foreach(target ${target_list})
+            message("   exmaning target: ${target}")
+
+            # get the target's type (executable, shared lib, static lib,...)
+            get_property(target_type
+                TARGET ${target}
+                PROPERTY TYPE
+            )
+            if(${target_type} MATCHES "_LIBRARY$" OR ${target_type} STREQUAL "EXECUTABLE")
+                message(STATUS "  target type is: ${target_type}")
+
+                # get the targets's sources
+                get_property(target_sources
+                    TARGET ${target}
+                    PROPERTY SOURCES
+                )
+
+                foreach(source ${target_sources})
+                    if(${source} MATCHES ".h")
+                        continue()
+                    endif()
+                    message(STATUS "   examining source ${source}")
+
+                    # read out all full header file paths (i.e. /../../../<header_name>.h) for each source file
+                    #get_filename_component(intermediate_source_path ${source} DIRECTORY)
+                    get_filename_component(intermediate_source_path ${source} PATH)
+                    get_filename_component(source_name_we ${source} NAME_WE)
+                    #set(header "${CMAKE_CURRENT_SOURCE_DIR}/${subdir}/${intermediate_source_path}/${source_name_we}.h")
+                    set(header "${subdir}/${intermediate_source_path}${source_name_we}.h")
+
+                    # now, look for exactly this header in the project
+                    message(STATUS "   looking for ${header}")
+                    if(EXISTS ${header})
+                        message(STATUS "   identified ${header}")
+
+                        # setup the header test source file (.cpp), i.e. the little main() programme including only
+                        # the current header, in the project's binary directory
+                        file(RELATIVE_PATH relative_header_path ${CMAKE_CURRENT_SOURCE_DIR}/.. ${header})
+                        #get_filename_component(relative_header_path ${relative_header_path} DIRECTORY)
+                        get_filename_component(relative_header_path ${relative_header_path} PATH)
+                        message(STATUS "   relative path from top level source directory is ${relative_header_path}")
+                        set(header_test_source_file ${CMAKE_CURRENT_BINARY_DIR}/${relative_header_path}/main_${source_name_we}.cpp)
+
+                        # create the cmake target corresponding to this main()-test programme
+                        set(header_test_target header_test_${relative_header_path}_${source_name_we})
+                        string(REPLACE "/" "_" header_test_target ${header_test_target})
+
+                        # if the target already exists comtinue lopp with next source file
+                        if(TARGET header_test_target)
+                            continue()
+                        endif()
+
+                        # else create file, include directories and link against libraries if neccessary
+                        message(STATUS "   Generating output file ${header_test_source_file}")
+                        file(GENERATE OUTPUT ${header_test_source_file}
+                             CONTENT "#include \"${header}\"\nint main()\n{\nreturn(0);\n}")
+
+
+
+
+                    else()
+                        message(STATUS "   could not find ${header}")
+                    endif()
+                endforeach()
+            endif()
+        endforeach()
+    endforeach()
+endfunction()
--- CMakeModules/QMoreProjectSettings.cmake
+++ CMakeModules/QMoreProjectSettings.cmake
@@ -1,9 +1,15 @@
 # *******************************************************************************************************************************
 # QtWidgetsProjectSettings
 #
-# This file contents all global project settings and defines useful variables for CMake
+# This file contains all global project settings and defines useful variables for CMake
 #
 # *******************************************************************************************************************************
+
+# Guard against multiple includes
+if(QMORE_TOOLCHAIN)
+    return()
+endif()
+
 
 # Recommanded global settings for projects based on C/C++ (a C++17 compiler is mandatory!)
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
@@ -14,9 +20,34 @@ set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 
-# find Qt package
+# Define our global W_TOOLCHAIN variable on which we
+# base all further build decisions. The variable W_TOOLCHAIN
+# is also available as a C/C++ preprocessor macro for
+# code, although it should only be used as a last resort there.
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    set(QMORE_TOOLCHAIN W_MSVC)
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    if(WIN32)
+        set(QMORE_TOOLCHAIN W_MINGW_NATIVE)
+    else()
+        set(QMORE_TOOLCHAIN W_GCC)
+    endif()
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    set(QMORE_TOOLCHAIN W_CLANG)
+endif()
+add_definitions(-D${QMORE_TOOLCHAIN})
+
+
+# Add global package QT
 find_package(QT NAMES Qt6 Qt5 COMPONENTS Widgets Gui LinguistTools REQUIRED)
 find_package(Qt${QT_VERSION_MAJOR} COMPONENTS Widgets Gui LinguistTools REQUIRED)
+
+set(W_USE_QT 5 CACHE STRING "Build optional components for given Qt version")
+set_property(CACHE W_USE_QT PROPERTY STRINGS 0 4 5)
+
+if(W_USE_QT STREQUAL "4")
+   message(AUTHOR_WARNING "Qt 4 is obsolete and not likely to be fully supported anymore.")
+endif()
 
 # set the Qt language
 set(TS_FILES libqmore_en_US.ts)
@@ -26,6 +57,10 @@ set(TS_FILES libqmore_en_US.ts)
 unset(QT_QMAKE_EXECUTABLE)
 
 
+
+
+
+# compiler flags
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall -Wextra -O0")
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Wall -Wextra -O2")
 
